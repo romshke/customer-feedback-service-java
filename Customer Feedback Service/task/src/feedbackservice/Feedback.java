0d8802b0f531ae447764d585d703808b973a6fee
romshke <romshkeept+github@gmail.com>
2025-09-05 01:10:58 +0300

    Initial commit

---
+++ Customer Feedback Service/task/src/feedbackservice/Feedback.java
@@ -0,0 +1,75 @@
+package feedbackservice;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "feedback")
+public class Feedback {
+    @Id
+    private ObjectId id;
+    private int rating;
+    private String feedback;
+    private String customer;
+    private String product;
+    private String vendor;
+
+    public Feedback() {
+    }
+
+    public Feedback(int rating, String feedback, String customer, String product, String vendor) {
+        this.rating = rating;
+        this.feedback = feedback;
+        this.customer = customer;
+        this.product = product;
+        this.vendor = vendor;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+}
---
+++ Customer Feedback Service/task/src/feedbackservice/RESTController.java
@@ -0,0 +1,41 @@
+package feedbackservice;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
+
+@Controller
+public class RESTController {
+    private final FeedbackRepository feedbackRepository;
+
+    public RESTController(FeedbackRepository feedbackRepository) {
+        this.feedbackRepository = feedbackRepository;
+    }
+
+    @PostMapping("/feedback")
+    public ResponseEntity<String> postFeedback(@RequestBody Feedback feedback) {
+        feedbackRepository.save(feedback);
+
+        URI uri = UriComponentsBuilder.fromPath("/feedback/{id}")
+                .buildAndExpand(feedback.getId())
+                .toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", uri.toString());
+
+        return ResponseEntity.created(uri).headers(headers).body(null);
+    }
+
+    @GetMapping("feedback/{id}")
+    public ResponseEntity<Optional<Feedback>> getFeedback(@PathVariable String id) {
+        return ResponseEntity.ok().body(feedbackRepository.findById(id));
+    }
+}
